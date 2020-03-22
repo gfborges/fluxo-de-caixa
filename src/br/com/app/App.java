1fd86d27b80edf6e69c47bca9143f8a01e6db6c1
gfborges <gabrielfr.borges@gmail.com>
2020-03-22 15:56:22 -0300

    menu options

--- src/br/com/app/App.java
+++ src/br/com/app/App.java
@@ -1,10 +1,38 @@
 package br.com.app;
 
+import br.com.negocio.Menu;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		while(true) {
+			int op = -1;
+			Menu.menu();
+			switch (op){
+				case 1: // Nova entrada
+					break;
+				case 2: // Nova saida
+					break;
+				case 3: // Editar entrada
+					break;
+				case 4: // Editar saida
+					break;
+				case 5: // Remover entrada
+					break;
+				case 6: // Remover saida
+					break;
+				case 7: // Relatório
+					break;
+				case 8: // Cadastrar usuário
+					break;
+				case 0:
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Opção invalida");
+			}
+		}
 	}
 
 }
--- src/br/com/negocio/Menu.java
+++ src/br/com/negocio/Menu.java
@@ -1,10 +1,16 @@
 package br.com.negocio;
 
 public class Menu {
-	static void Menu() {
+	public static void menu() {
 		System.out.println("Menu");
-		System.out.println("0. Nova entrada");
-		System.out.println("1. Nova saida");
-		System.out.println("");
+		System.out.println(" [1] Nova entrada");
+		System.out.println(" [2] Nova saida");
+		System.out.println(" [3] Excluir entrada");
+		System.out.println(" [4] Excluir saida");
+		System.out.println(" [5] Editar entrada");
+		System.out.println(" [6] Editar saida");
+		System.out.println(" [7] Relatorios");
+		System.out.println(" [8] Cadastrar usuário");
+		System.out.println(" [0] Sair");
 	}
 }
