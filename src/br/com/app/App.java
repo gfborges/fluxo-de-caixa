6efa1506f9a996a8b8fab0d323d0c61b462810a9
gfborges <gabrielfr.borges@gmail.com>
2020-03-22 19:54:44 -0300

    nova entrada e nova saida

--- src/br/com/app/App.java
+++ src/br/com/app/App.java
@@ -1,18 +1,24 @@
 package br.com.app;
 
+import br.com.modelo.Caixa;
 import br.com.negocio.Menu;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Menu menu= new Menu();
+		Caixa caixa = new Caixa();
+		int op = -1;
+		
 		while(true) {
-			int op = -1;
-			Menu.menu();
+			op = menu.menu();
 			switch (op){
 				case 1: // Nova entrada
+					menu.nova_entrada(caixa);
 					break;
 				case 2: // Nova saida
+					menu.nova_saida(caixa);
 					break;
 				case 3: // Editar entrada
 					break;
@@ -27,11 +33,12 @@ public class App {
 				case 8: // Cadastrar usuário
 					break;
 				case 0:
-					System.exit(0);
+					menu.sair();
 					break;
 				default:
 					System.out.println("Opção invalida");
 			}
+			menu.pausar();
 		}
 	}
 
--- src/br/com/negocio/Menu.java
+++ src/br/com/negocio/Menu.java
@@ -1,7 +1,21 @@
 package br.com.negocio;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import br.com.modelo.Caixa;
+import br.com.modelo.Entrada;
+import br.com.modelo.Saida;
+import br.com.modelo.Transacao;
+import br.com.negocio.Controle;
+
 public class Menu {
-	public static void menu() {
+	private Controle ctrl;
+	
+	public Menu() {
+		this.ctrl = new Controle();
+	}
+	
+	public int menu() {
 		System.out.println("Menu");
 		System.out.println(" [1] Nova entrada");
 		System.out.println(" [2] Nova saida");
@@ -12,5 +26,113 @@ public class Menu {
 		System.out.println(" [7] Relatorios");
 		System.out.println(" [8] Cadastrar usuário");
 		System.out.println(" [0] Sair");
+		System.out.print("Selecione uma opçao: ");
+		return ctrl.opcao();
 	}
+	
+	private int ler_tipo(String[] opcoes) {
+		int tipo;
+		do {
+			System.out.println("Escolha um dos tipos a seguir:");
+			Transacao.listar_tipos(opcoes); // lista comecando do 1
+			System.out.print("Tipo: ");
+			tipo = ctrl.opcao();
+		}while(tipo < 0 || tipo >= Entrada.TIPOS.length );
+		return tipo - 1; // indice comeca do 0
+	}
+	
+	private double ler_valor() {
+		double valor;
+		do {
+			System.out.print("Valor: ");
+			valor = ctrl.valor();
+		}while(valor  <  0);
+		return valor;
+	}
+	
+	private String ler_data() {
+		String data;
+		LocalDate dt = LocalDate.now();
+		boolean dt_flag;
+		do {
+			dt_flag = false;
+			System.out.print("Data(aaaa-MM-dd): ");
+			data = ctrl.texto();
+			if(!data.isEmpty()) {
+				try {
+					dt = LocalDate.parse(data); 
+				}
+				catch( DateTimeParseException e){
+					System.out.println("Data invalida! Tente novamente");
+					dt_flag = true;
+				}
+			}
+			else {
+				return dt.toString(); // data de hoje
+			}
+		}while(dt_flag);
+		return data;
+	}
+	
+	public void nova_entrada(Caixa caixa) {
+		String data, ano, mes, semana;
+		LocalDate dt = LocalDate.now();
+		int tipo = -1;
+		double valor = -1;
+		System.out.println("Por favor preencha todos os campos");
+		
+		// Tipo
+		tipo = ler_tipo(Entrada.TIPOS);
+		
+		// Valor
+		valor = ler_valor();
+		
+		// Data
+		data = ler_data();
+		
+		Transacao ent = new Entrada(data, valor, tipo);
+		
+		ano = Integer.toString( dt.getYear() );
+		mes = String.format("%02d", dt.getMonthValue() );
+		semana = String.format("%02d", dt.getDayOfMonth() / 7 );
+		String key = String.format("%s-%s-%s", ano, mes, semana);
+		
+		caixa.novo_registro(key, ent);
+	}
+	
+	public void nova_saida(Caixa caixa) {
+		String data, ano, mes, semana;
+		LocalDate dt = LocalDate.now();
+		int tipo = -1;
+		double valor = -1;
+		System.out.println("Por favor preencha todos os campos a seguir");
+		
+		// Tipo
+		tipo = ler_tipo(Saida.TIPOS);
+		
+		// Valor
+		valor = ler_valor();
+		
+		// Data
+		data = ler_data();
+		
+		ano = Integer.toString( dt.getYear() );
+		mes = String.format("%02d", dt.getMonthValue() );
+		semana = String.format("%02d", dt.getDayOfMonth() / 7 );
+		
+		Transacao ent = new Saida(data, valor, tipo);
+		String key = String.format("%s-%s-%s", ano, mes, semana);
+		
+		caixa.novo_registro(key, ent);
+	}
+	
+	public void pausar() {
+		System.out.print("\nPressione enter para continuar...");
+		ctrl.texto();
+	}
+	
+	public void sair() {
+		System.exit(0);
+	}
+	
 }
