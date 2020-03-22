1650542020f071dcf3c7a0518440558c152ec348
gfborges <gabrielfr.borges@gmail.com>
2020-03-22 14:25:39 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fluxo de caixa
+Um fluxo de caixa feito como atividade para aula de programação orientada a objeto 
+
+# Autores
+* (Gabriel Borges)[https://github.com/gfborges]
+* (Gabriel Mendes)[https://github.com/gmendess]
+
---
+++ src/br/com/negocio/Menu.java
@@ -0,0 +1,10 @@
+package br.com.negocio;
+
+public class Menu {
+	static void Menu() {
+		System.out.println("Menu");
+		System.out.println("0. Nova entrada");
+		System.out.println("1. Nova saida");
+		System.out.println("");
+	}
+}
