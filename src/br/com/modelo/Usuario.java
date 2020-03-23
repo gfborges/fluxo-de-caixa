b0f95497da7f4682123d933c96f4210c75664cc0
gfborges <gabrielfr.borges@gmail.com>
2020-03-23 17:10:34 -0300

    Adicionado tipo em usuario como boolean; metodo contrutor de usuario fromCSV

--- src/br/com/modelo/Usuario.java
+++ src/br/com/modelo/Usuario.java
@@ -6,8 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
+import br.com.negocio.Controle;
+
 public class Usuario {
 	public static final String ARQUIVO_USUARIOS = "usuarios.csv";
+	public static final String[] TIPOS = {"Pessoa Juridica", "Pessoa Fisica"};
 	
 	private final String LOGIN;
 	private final String SENHA;
@@ -15,17 +18,18 @@ public class Usuario {
 	
 	private String nome;
 	private String email;
-	private boolean tipo; // PJ (T) / PF (F)
+	private boolean tipo; // PF (F) / PJ (T)
 	
 	private Telefone tel;
 	private Endereco endereco;
 	
-	public Usuario(String LOGIN, String SENHA, String caixa, String nome, String email, Telefone tel, Endereco endereco) {
+	public Usuario(String LOGIN, String SENHA, String caixa, String nome, String email, int tipo, Telefone tel, Endereco endereco) {
 		this.LOGIN = LOGIN;
 		this.SENHA = SENHA;
 		this.caixa = caixa;
 		this.nome = nome;
 		this.email = email;
+		this.tipo = (tipo == 1)? true : false;
 		this.tel = tel;
 		this.endereco = endereco;
 	}
@@ -78,14 +82,27 @@ public class Usuario {
 		return SENHA;
 	}
 	
+	public boolean isPj(){
+		return tipo;
+	}
+	
+	public boolean isPf() {
+		return !tipo;
+	}
+	
+	public int getTipo_int() {
+		return (tipo)? 1: 0;
+	}
+	
 	public void toCSV() throws IOException {
 		boolean saving = true;
 		String line  = LOGIN + "," +
 					   SENHA + "," +
 					   caixa + "," +
-					   nome + "," +
+					   nome  + "," +
 					   email + "," +
-					   tel.getNumero() + "," +
+					   getTipo_int() + "," +
+					   Controle.attrOuEspaco(tel.getNumero()) + "," +
 					   endereco.toCSV() + "\n";
 		while(saving) {
 			try {
@@ -97,5 +114,19 @@ public class Usuario {
 			}
 			saving = !saving;
 		}
+	}
+	
+	public static Usuario fromCSV(String[] row) {
+		if(row.length != 12)
+			System.err.println("Erro na leitura do arquivo");
+		String login = row[0];
+		String senha = row[1];
+		String caixa = row[2];
+		String nome  = row[3];
+		String email = row[4];
+		int tipo = Integer.parseInt(row[5]);
+		Telefone tel = new Telefone(row[6].trim());
+		Endereco end = new Endereco(row[7].trim(), row[8].trim(), row[9].trim(), row[10].trim(), row[11].trim());
+		return new Usuario(login, senha, caixa, nome, email, tipo, tel, end);
 	}
 }
