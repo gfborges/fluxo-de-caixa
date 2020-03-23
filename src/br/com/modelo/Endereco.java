ee48a6a3c69a126a2fb8f997ad1969b2ff98c7ec
gfborges <gabrielfr.borges@gmail.com>
2020-03-23 11:40:41 -0300

    classe Endereco

---
+++ src/br/com/modelo/Endereco.java
@@ -0,0 +1,59 @@
+package br.com.modelo;
+
+public class Endereco {
+	private String cidade;
+	private String bairro;
+	private String rua;
+	private String numero;
+	private String complemento;
+	
+	public Endereco(String cidade, String bairro, String rua, String numero, String complemento) {
+		super();
+		this.cidade = cidade;
+		this.bairro = bairro;
+		this.rua = rua;
+		this.numero = numero;
+		this.complemento = complemento;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+}
