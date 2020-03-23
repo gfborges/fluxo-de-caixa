b73db252a66323917c73bf821438582c6330445b
gfborges <gabrielfr.borges@gmail.com>
2020-03-23 18:12:35 -0300

    adicão das vairavei de tipo(pf/pj)

--- src/br/com/modelo/Saida.java
+++ src/br/com/modelo/Saida.java
@@ -1,10 +1,11 @@
 package br.com.modelo;
 
 public class Saida extends Transacao{
-	  public final static String[] TIPOS =  {"Energia Elétrica", "Gás", "Alimentacao (refeitório)", "Alimentacao(fora de casa)", "Combustível", "Outros" };
+	  public final static String[] TIPOS_PF =  {"Energia Elétrica", "Gás", "Alimentacao", "Combustível", "Outros" };
+	  public final static String[] TIPOS_PJ =  {"Energia Elétrica", "Gás", "Alimentacao (refeitorio)", "Combustível", "Outros" };
 	  
-	  public Saida(String data,double valor, int tipo){
-	    super(data, valor, false, Saida.TIPOS[tipo]);
+	  public Saida(String data,double valor, String tipo){
+	    super(data, valor, false, tipo);
 	  }
 
 	  public String  toString(){
